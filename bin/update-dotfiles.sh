8b9a5cf4116d82574c3c1d788bb565a114e176ef
Kylras <miguel.gotor.r@gmail.com>
2023-08-08 14:51:07 +0200

    Add dir /etc/

--- bin/update-dotfiles.sh
+++ bin/update-dotfiles.sh
@@ -1,33 +1,46 @@
 #!/bin/sh
 # Update local git repo by copying those files that have changed
 # Ensure the script is executed from $HOME
+set -e
 cd "$HOME" || exit
+
+
+dotfiles_dir="./dotfiles"
 
 # Use relative paths to prevent the "--parents" option from copying unnnecesary
 # parent directories (/home/user/...) while preserving directory structure:
 
-cp --parents -u "./.config/Code - OSS/User/settings.json" "./dotfiles"
-cp --parents -ru "./.config/cava" "./dotfiles"
-cp --parents -ru "./.config/dunst" "./dotfiles"
-cp --parents -ru "./.config/gtk-2.0" "./dotfiles"
-cp --parents -ru "./.config/gtk-3.0" "./dotfiles"
-cp --parents -ru "./.config/gtk-4.0" "./dotfiles"
-cp --parents -ru "./.config/i3" "./dotfiles"
-cp --parents -ru "./.config/i3blocks" "./dotfiles"
-cp --parents -ru "./.config/i3status" "./dotfiles"
-cp --parents -ru "./.config/kitty" "./dotfiles"
-cp --parents -ru "./.config/leafpad" "./dotfiles"
-cp --parents -ru "./.config/libfm" "./dotfiles"
-cp --parents -ru "./.config/mpv" "./dotfiles"
-cp --parents -ru "./.config/pcmanfm" "./dotfiles"
-cp --parents -ru "./.config/picom" "./dotfiles"
-cp --parents -ru "./.config/ranger" "./dotfiles"
-cp --parents -ru "./.config/vim" "./dotfiles"
-cp --parents -ru "./.config/X11" "./dotfiles"
-cp --parents -ru "./.config/zsh" "./dotfiles"
-cp --parents -ru "./.config/wgetrc" "./dotfiles"
-cp --parents -ru "./bin" "./dotfiles"
-cp --parents -ru "./Pictures/Wallpapers" "./dotfiles"
-cp --parents -ru "./.profile" "./dotfiles"
-cp --parents -ru "./.mozilla/firefox/hl9gcn50.default-release/chrome/userChrome.css" "./dotfiles"
+cp --parents -u "./.config/Code - OSS/User/settings.json" $dotfiles_dir
+cp --parents -ru "./.config/cava" $dotfiles_dir
+cp --parents -ru "./.config/dunst" $dotfiles_dir
+cp --parents -ru "./.config/gtk-2.0" $dotfiles_dir
+cp --parents -ru "./.config/gtk-3.0" $dotfiles_dir
+cp --parents -ru "./.config/gtk-4.0" $dotfiles_dir
+cp --parents -ru "./.config/i3" $dotfiles_dir
+cp --parents -ru "./.config/i3blocks" $dotfiles_dir
+cp --parents -ru "./.config/i3status" $dotfiles_dir
+cp --parents -ru "./.config/kitty" $dotfiles_dir
+cp --parents -ru "./.config/leafpad" $dotfiles_dir
+cp --parents -ru "./.config/libfm" $dotfiles_dir
+cp --parents -ru "./.config/mpv" $dotfiles_dir
+cp --parents -ru "./.config/pcmanfm" $dotfiles_dir
+cp --parents -ru "./.config/picom" $dotfiles_dir
+cp --parents -ru "./.config/ranger" $dotfiles_dir
+cp --parents -ru "./.config/vim" $dotfiles_dir
+cp --parents -ru "./.config/X11" $dotfiles_dir
+cp --parents -ru "./.config/zsh" $dotfiles_dir
+cp --parents -ru "./.config/wgetrc" $dotfiles_dir
+cp --parents -ru "./bin" $dotfiles_dir
+cp --parents -ru "./Pictures/Wallpapers" $dotfiles_dir
+cp --parents -ru "./.profile" $dotfiles_dir
+cp --parents -ru "./.mozilla/firefox/hl9gcn50.default-release/chrome/userChrome.css" $dotfiles_dir
+
+# System-wide config files worth tracking.
+# Not dotfiles per se, but modified to make LightDM XDG BDS compliant
+cp --parents -ru "/etc/lightdm/Xsession" $dotfiles_dir
+cp --parents -ru "/etc/security/pam_env.conf" $dotfiles_dir
+
 echo "dotfiles updated"
+$TERMINAL --working-directory $dotfiles_dir
+
+
