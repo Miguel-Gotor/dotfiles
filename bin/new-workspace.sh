545d2e5dd267dad07e54b7a8d8e9ac1cd370855b
Kylras <miguel.gotor.r@gmail.com>
2023-08-09 19:39:15 +0200

    Beautified. Add explanatory comments

--- bin/new-workspace.sh
+++ bin/new-workspace.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
-# v1 (From user https://github.com/sandeel)
+# new-workspace.sh
+# Create a new workspace next to the highest one
+
+# V1: From user https://github.com/sandeel
 i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
 
-# v2 using jq (much slower tho)
+# V2: Using jq (much slower tho)
 # i3-msg "workspace $(i3-msg -t get_workspaces | jq 'max_by(.num).num + 1')"
 
--- bin/playerctl-follow.sh
+++ bin/playerctl-follow.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 # One liner command to notify updates in the media player
-
-playerctl -F metadata mpris:artUrl | while read -r thumbnail; do \
-    [ -z "$thumbnail" ] ||  dunstify -i $thumbnail -h "string:x-dunst-stack-tag:"mpris"" \
-    "$(playerctl metadata artist)" "\n<span  color='#0FFFFF' font='18px'>$(playerctl metadata title)</span>"; done
-
 # Assuming the URL points to a local file, as dunst expects the absolute file path
 # file:///home/user/.mozilla/firefox/firefox-mpris/123.png
-
 # Otherwise, the URL must be curled and temporarily stored
 # https://i1.ytimg.com/vi/qFfybn_W8Ak/hqdefault.jpg
 # temp_file=$(mktemp)
 # curl -s $url -o "$temp_file"
-# ...
+
+playerctl -F metadata mpris:artUrl | while read -r thumbnail; do \
+[ -z "$thumbnail" ] ||  dunstify -i $thumbnail -h "string:x-dunst-stack-tag:"mpris"" \
+"$(playerctl metadata artist)" "\n<span  color='#0FFFFF' font='18px'>$(playerctl metadata title)</span>"; done
