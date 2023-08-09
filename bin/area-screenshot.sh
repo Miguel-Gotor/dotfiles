5f429d6c8fb8753191f7ce24c823898f5478863d
Kylras <miguel.gotor.r@gmail.com>
2023-08-09 19:37:35 +0200

    Add URL link to the img file in the notification

--- bin/area-screenshot.sh
+++ bin/area-screenshot.sh
@@ -8,6 +8,6 @@ msg_tag=$0
 temp_file=$(mktemp /tmp/screenshot-XXX.png)
 maim -u -c 1,1,1 -s $temp_file
 cat $temp_file | xclip -selection clipboard -t image/png
-dunstify -i $temp_file 'Snippet copied to clipboard'
-rm $temp_file
+dunstify -i $temp_file 'Snippet copied to clipboard' "<a href=\"file://$temp_file\"></a>"
+# rm $temp_file
 paplay /usr/share/sounds/freedesktop/stereo/message.oga
