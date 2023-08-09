3b8dee23ce2aadb9fb288ab04d8ceb2ccb09d2f1
Kylras <miguel.gotor.r@gmail.com>
2023-08-09 19:23:14 +0200

    Add default action on LMB click

---
+++ bin/notify-metadata-action.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# notify-metadata-action.sh
+# Should be called on player metadata update
+# Share tag with other MPRIS notifications since they all carry the same info
+# Click notification to "raise" (focus) the current player
+# Lazy eval: If notification is clicked, send dbus method call
+# Else, exit with 0 code.
+
+[  "$(dunstify -A 0,OPEN \
+-i  $(playerctl metadata mpris:artUrl) \
+-h  "string:x-dunst-stack-tag:"mpris"" \
+    "$(playerctl metadata artist)" \
+    "\n<span  color='#0FFFFF' font='18px'> \
+    $(playerctl metadata title)</span>";)" = "0" ] && 
+    
+    dbus-send --type=method_call \
+             --dest=org.mpris.MediaPlayer2.playerctld \
+             /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Raise ||
+
+    exit 0
