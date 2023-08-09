875042c14b12c58e36805cc16e561bcceb637d00
Kylras <miguel.gotor.r@gmail.com>
2023-08-09 19:38:24 +0200

    Beautified. Add explanatory comments

--- bin/calendar.sh
+++ bin/calendar.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 # calendar.sh
-# Highlight current date in calendar using "cal"
+# Notify current date in calendar using "cal"
 # Unique identifier (name of the script for example)
-msg_tag=$0
-
-# Uncolored calendar output
-cal=$(cal)
-
-# Highlight the current in yellow
+# Color today's date in yellow
 # Use word boundaries in the regular expression to ensure that only the 
-# standalone day of the month is matched, preventing partial matching of day numbers like 1 in 11, 12...
+# standalone day of the month is matched, preventing partial matching of day
+# numbers like 1 in 11, 12...
+
+cal=$(cal)
 highlighted_output=$(echo "$cal" | sed "s/\b$(date +%e | sed 's/ //g')\b/<span color='yellow'>\\0<\/span>/")
 
-# Notification
-dunstify -i /usr/share/icons/breeze-dark/apps/48/office-calendar.svg -h "string:x-dunst-stack-tag:$msg_tag" \
-"Calendar" "\n<span font_family='Hack' font='15px'>$highlighted_output\n</span>"
+dunstify -i /usr/share/icons/breeze-dark/apps/48/office-calendar.svg \
+        -h "string:x-dunst-stack-tag:$0" \
+        "Calendar" "\n<span font_family='Hack' font='15px'>$highlighted_output\n</span>"
