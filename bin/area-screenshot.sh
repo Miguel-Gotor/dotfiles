7f0982e7560a06a9d6573b9b8f87366084c62dfd
Kylras <miguel.gotor.r@gmail.com>
2023-08-15 12:05:54 +0200

    Replaced maim with scrot which supports freeze option

--- bin/area-screenshot.sh
+++ bin/area-screenshot.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # area-screenshot.sh
 # Take a screenshot of the selected area
-
+# Click the notification to open the file using the default image viewer
 # Unique identifier (name of the script for example)
-msg_tag=$0
+msg_tag=$(basename $0)
 
 temp_file=$(mktemp /tmp/screenshot-XXX.png)
-maim -u -c 1,1,1 -s $temp_file
+# maim -u -c 1,0,0 -s $temp_file
+
+scrot -o -s -f $temp_file
 cat $temp_file | xclip -selection clipboard -t image/png
 dunstify -i $temp_file 'Snippet copied to clipboard' "<a href=\"file://$temp_file\"></a>"
 # rm $temp_file
