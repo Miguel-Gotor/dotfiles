0fcbc087cde97e5431d0cc122f8ba9c79a8a46d7
Kylras <miguel.gotor.r@gmail.com>
2023-08-15 12:10:53 +0200

    Add comments

--- bin/dbus-focus-player.sh
+++ bin/dbus-focus-player.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# Bindsym to key $mod+p (player)
+# We can also call this script on notification click.
+# See "notify-metadata-action.sh"
 dbus-send --type=method_call \
              --dest=org.mpris.MediaPlayer2.playerctld \
              /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Raise
