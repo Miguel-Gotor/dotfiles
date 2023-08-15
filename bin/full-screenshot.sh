78957cd1fc0e06c5383efcfad8883970e34568f5
Kylras <miguel.gotor.r@gmail.com>
2023-08-15 12:12:49 +0200

    Beautified. Replaced scrot with maim (faster)

--- bin/full-screenshot.sh
+++ bin/full-screenshot.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 # full-screenshot.sh
 # Take a screenshot of the entire screen using scrot
+
 # Unique identifier (name of the script for example)
-msg_tag=$0
+msg_tag=$(basename $0)
 
 directory="$HOME/Pictures/Screenshots"
 filename=$(date "+%d-%m-%Y-%T.png")
+path="$directory/$filename"
 body="<span font_family='Hack' color='#57dafd' font='14px'>$filename</span>"
 
-# Generate the filename as a Unix epoch timestamp
-# filename=$(date +%s).png
+dunstify \
+    -i video-display \
+    -h "string:x-dunst-stack-tag:img" \
+    "Taking screenshot..." &
 
-dunstify -i video-display -h "string:x-dunst-stack-tag:img" \
-"Taking screenshot..."
-
-scrot "$directory/$filename"
+maim -o $file"
 
 # Notification
-dunstify -i $directory/$filename -h "string:x-dunst-stack-tag:$msg_tag" \
-"Screenshot saved as:" "$body"
+dunstify \
+    -i $directory/$filename \
+    -h "string:x-dunst-stack-tag:$msg_tag" \
+    "Screenshot saved as:" "$body" \
+    "<a href=\"file://$path\"></a>"
 
-paplay /usr/share/sounds/freedesktop/stereo/camera-shutter.oga
+paplay /usr/share/sounds/freedesktop/stereo/camera-shutter.oga &
