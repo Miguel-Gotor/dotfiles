38c58124d8a6ab7620439bf4dbebc699e6722cd1
Kylras <miguel.gotor.r@gmail.com>
2023-08-30 22:00:53 +0200

    Replaced scrot with maim. Scrot does not unhide the cursor before the selection (unclutter)

--- bin/area-screenshot.sh
+++ bin/area-screenshot.sh
@@ -6,9 +6,9 @@
 msg_tag=$(basename $0)
 
 temp_file=$(mktemp /tmp/screenshot-XXX.png)
-# maim -u -c 1,0,0 -s $temp_file
+maim -u -c 1,0,0 -s $temp_file
 
-scrot -o -s -f $temp_file
+# scrot -o -s -f $temp_file
 cat $temp_file | xclip -selection clipboard -t image/png
 dunstify -i $temp_file 'Snippet copied to clipboard' "<a href=\"file://$temp_file\"></a>"
 # rm $temp_file
